c4b5b4eb4ea2c4806caaee0b3692ca910c99f0e6
Precious <ikpaprecious2@gmail.com>
2022-07-12 12:38:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom"
+import NavBar from "./Components/NavBar"
+import Home from "./Pages/Home"
+
+
+
+
+
+
+function App() {
+ 
+
+  return (
+    <div>
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+    </div>
+      
+   
+  )
+}
+
+export default App
